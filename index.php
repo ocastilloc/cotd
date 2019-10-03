fbed0f2a11103613da8b424843998d541b2febd6
root <oscar.rodrigo.castillo@gmail.com>
2019-10-03 16:27:37 -0300

    Cambio realizado

--- index.php
+++ index.php
@@ -4,12 +4,11 @@ Blog: https://StefanoPicozzi.blog
 GitHub: https://github.com/StefanoPicozzi/cotd.git
 Date: 2016
 -->
-
+<script>alert('Hola mundo')</script>
 <?php
 
 session_start();
 include('include/selector.php');
 
 header( 'Location: item.php' );
-echo "Hola mundo";
 ?>
